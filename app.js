894b89f6c2c4911f961149a5fa10d33a766d4c20
tejsubedi@gmail.com <tejsubedi@gmail.com>
2019-10-09 18:58:42 -0400

    added morgan middleware

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
-//Bringing routes
-const postRoutes = require('./routes/post');
 
-app.get('/', postRoutes.getPosts);
+//Bringing routes
+const { getPosts } = require('./routes/post');
+
+const myOwnMiddleware = () => {
+    console.log(`Middleware works`);
+};
+
+
+//Middleware
+app.use(morgan("dev"));
+app.use(myOwnMiddleware);
+
+app.get('/', getPosts);
 
 const PORT = 8080;
 
