9c9a2a1db9cadd1684ed206bca27eeacf4664c0d
tejsubedi@gmail.com <tejsubedi@gmail.com>
2019-10-09 20:49:24 -0400

    adding post data

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const mongoose = require('mongoose');
 dotenv.config();
 
 
-//DB
+//DB connection 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true }).then(() => console.log(`DB connected`));
 
 mongoose.connection.on('error', err => {
