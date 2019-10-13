35943b03ead0f052fdcd297d0e975b3305b82921
tejsubedi@gmail.com <tejsubedi@gmail.com>
2019-10-13 12:02:44 -0400

    added express validator

--- controllers/post.js
+++ controllers/post.js
@@ -13,13 +13,13 @@ exports.createPost = (req, res) => {
     post.save((err, result) => {
         if (err) {
             return res.status(400).json({
-    //             error: err
-    //         })
-    //     }
-    //     res.status(200).json({
-    //         post: result
-    //     })
-    // })
+                error: err
+            })
+        }
+        res.status(200).json({
+            post: result
+        })
+    })
 
     post.save().then(result => {
         res.status(200).json({
