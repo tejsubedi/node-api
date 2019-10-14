df266418092b85a887f74d2b33109e02d8028ad0
tejsubedi@gmail.com <tejsubedi@gmail.com>
2019-10-13 22:30:45 -0400

    added express validator successfully

--- models/post.js
+++ models/post.js
@@ -3,15 +3,15 @@ const mongoose = require('mongoose');
 const postSchema = new mongoose.Schema({
     title: {
         type: String,
-        required: "Title is required",
-        minlength: 4,
-        maxlength: 150
+        required: true,
+        // minlength: 4,
+        // maxlength: 150
     },
     body: {
         type: String,
-        required: "Body is required",
-        minlength: 4,
-        maxlength: 1500
+        required: true,
+        // minlength: 4,
+        // maxlength: 1500
     }
 });
 
