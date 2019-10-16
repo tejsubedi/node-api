146e0a20de0e58431dc18e18984fb536e0ef02da
tejsubedi@gmail.com <tejsubedi@gmail.com>
2019-10-16 18:38:10 -0400

    added user schema

---
+++ models/user.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const uuidv1 = require('uuid/v1');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    email: {
+        type: String,
+        trim: true,
+        required: true,
+    },
+    hashed_password: {
+        type: String,
+        required: true
+    },
+    salt: String,
+    created: {
+        type: Date,
+        default: Date.now
+    },
+    updated: Date
+});
+
+
+
+
+
+module.exports = mongoose.model("User", userSchema);
--- validator/index.js
+++ validator/index.js
@@ -8,7 +8,7 @@ exports.createPostValidator = (req, res, next) => {
 
     //body
     req.check('body', "Write a body").notEmpty()
-    req.check('body', "Body must be betweenn 4 to 1500 characters").isLength({
+    req.check('body', "Body must be betweenn 4 to 2000 characters").isLength({
         min: 4,
         max: 2000
     });
