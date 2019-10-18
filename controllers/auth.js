46b6dae42a938aacdf933441655a641707352fae
tejsubedi@gmail.com <tejsubedi@gmail.com>
2019-10-17 22:13:44 -0400

    added auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,4 +5,7 @@ exports.signup = async (req, res) => {
     if(userExists) return res.status(403). json({
         error: "Email is taken!"
     })
+    const user = await new User(req.body)
+    await user.save()
+    res.status(200).json({user});
 }
