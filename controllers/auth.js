862fa478c63835d7e886c3aacd7109b29979fc0c
tejsubedi@gmail.com <tejsubedi@gmail.com>
2019-10-17 22:12:01 -0400

    added auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,8 @@
 const User = require('../models/user');
 
-exports.signup = (req, res) => {
-    const userExists = 
+exports.signup = async (req, res) => {
+    const userExists = await User.findOne({email: req.body.email})
+    if(userExists) return res.status(403). json({
+        error: "Email is taken!"
+    })
 }
