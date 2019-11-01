2ac178cd7ee6f181119e70022b9597a7bc614883
tejsubedi@gmail.com <tejsubedi@gmail.com>
2019-11-01 18:00:31 -0400

    adding user controller

--- routes/auth.js
+++ routes/auth.js
@@ -8,6 +8,8 @@ const router = express.Router();
 router.post('/signup', userSignupValidator, signup);
 router.post('/signin', signin);
 router.get('/signout', signout);
+
+//any route containing : userId, our app will first execute userById()
 router.param("userId", userById);
 
 module.exports = router;
