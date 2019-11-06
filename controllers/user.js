134ef8bc535db78da1252c82cdc76870a2d34228
tejsubedi@gmail.com <tejsubedi@gmail.com>
2019-11-05 20:25:01 -0500

    added authorised

--- controllers/user.js
+++ controllers/user.js
@@ -1,13 +1,22 @@
 const User = require('../models/user');
 
-exports.userById = (req, res, next ) => {
+exports.userById = (req, res, next) => {
     User.findById(id).exec((err, user) => {
-        if(err || !user){
+        if (err || !user) {
             return res.status(400).json({
                 error: "User not found"
             })
         }
         req.profile = user //adds profile object in req with user info
         next()
-    })
-}
+    });
+};
+
+exports.hasAuthorization = (req, res, next) => {
+    const authorised = req.profile && req.auth && req.profile._id === req.auth._id;
+    if (!authorised) {
+        return res.status(403).json({
+            error: "User is not authorised to perform this action"
+        });
+    }
+};
