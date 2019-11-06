9e464b8e1cb7a0c8afea3073cd018f567a24afa5
tejsubedi@gmail.com <tejsubedi@gmail.com>
2019-11-05 20:26:15 -0500

    added authorised

--- routes/post.js
+++ routes/post.js
@@ -6,8 +6,8 @@ const { createPostValidator } = require('../validator');
 
 const router = express.Router();
 
-router.get('/', requireSignin, getPosts);
-router.post('/post', createPostValidator, createPost);
+router.get('/',  getPosts);
+router.post('/post', requireSignin, createPostValidator, createPost);
 
 //any route containing : userId, our app will first execute userById()
 router.param("userId", userById);
