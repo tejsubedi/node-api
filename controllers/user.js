fcd74ab3ec0136f38f091f8eabd522553adaa15a
tejsubedi@gmail.com <tejsubedi@gmail.com>
2019-11-08 22:53:23 -0500

    added user controller

--- controllers/user.js
+++ controllers/user.js
@@ -20,3 +20,14 @@ exports.hasAuthorization = (req, res, next) => {
         });
     }
 };
+
+exports.allUsers = (req, res) => {
+    User.find((err, users) => {
+        if(err) {
+            return res.status(400).json({
+                error: err
+            })
+        }
+        res.json({users});
+    }).select("name email updated created");
+}
